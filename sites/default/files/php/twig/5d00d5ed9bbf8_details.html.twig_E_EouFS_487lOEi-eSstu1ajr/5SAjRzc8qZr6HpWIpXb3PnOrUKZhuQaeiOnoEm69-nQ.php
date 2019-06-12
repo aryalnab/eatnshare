5ebd005e8e06d6c57e29635d32cd042d23e5cb93
aryalnab <nabin.aryal@outlook.com>
2019-06-12 13:45:55 +0300

    added

---
+++ sites/default/files/php/twig/5d00d5ed9bbf8_details.html.twig_E_EouFS_487lOEi-eSstu1ajr/5SAjRzc8qZr6HpWIpXb3PnOrUKZhuQaeiOnoEm69-nQ.php
@@ -0,0 +1,131 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/custom/mytheme/templates/form/details.html.twig */
+class __TwigTemplate_edf612b46e0ea37439172c71f8238d8c45abf925428172c945a819ec1a19b984 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 18];
+        $filters = ["escape" => 17];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 17
+        echo "<details";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null)), "html", null, true);
+        echo ">";
+        // line 18
+        if (($context["title"] ?? null)) {
+            // line 19
+            echo "<summary";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["summary_attributes"] ?? null)), "html", null, true);
+            echo ">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "</summary>";
+        }
+        // line 21
+        echo "<div class=\"details-wrapper\">
+    ";
+        // line 22
+        if (($context["errors"] ?? null)) {
+            // line 23
+            echo "      <div class=\"form-item--error-message\">
+        <strong>";
+            // line 24
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["errors"] ?? null)), "html", null, true);
+            echo "</strong>
+      </div>
+    ";
+        }
+        // line 27
+        if (($context["description"] ?? null)) {
+            // line 28
+            echo "<div class=\"details-description\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null)), "html", null, true);
+            echo "</div>";
+        }
+        // line 30
+        if (($context["children"] ?? null)) {
+            // line 31
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["children"] ?? null)), "html", null, true);
+        }
+        // line 33
+        if (($context["value"] ?? null)) {
+            // line 34
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["value"] ?? null)), "html", null, true);
+        }
+        // line 36
+        echo "</div>
+</details>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/mytheme/templates/form/details.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  99 => 36,  96 => 34,  94 => 33,  91 => 31,  89 => 30,  84 => 28,  82 => 27,  76 => 24,  73 => 23,  71 => 22,  68 => 21,  61 => 19,  59 => 18,  55 => 17,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/custom/mytheme/templates/form/details.html.twig", "/var/www/html/projectwork/themes/custom/mytheme/templates/form/details.html.twig");
+    }
+}
