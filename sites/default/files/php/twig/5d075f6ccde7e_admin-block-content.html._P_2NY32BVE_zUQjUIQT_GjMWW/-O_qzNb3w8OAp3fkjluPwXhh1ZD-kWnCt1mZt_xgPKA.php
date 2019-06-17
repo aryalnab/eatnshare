efa9cccda975c9b2647d3db4fbf92479538c1f04
kecyGaurab <kecy.gaurab@gmail.com>
2019-06-17 14:41:25 +0300

    another commit

---
+++ sites/default/files/php/twig/5d075f6ccde7e_admin-block-content.html._P_2NY32BVE_zUQjUIQT_GjMWW/-O_qzNb3w8OAp3fkjluPwXhh1ZD-kWnCt1mZt_xgPKA.php
@@ -0,0 +1,120 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/admin/admin-block-content.html.twig */
+class __TwigTemplate_f8a08b46fd61bebb882284bbcdd650459064d63aed555aa48d9cc2683b255ab6 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 18, "if" => 23, "for" => 25];
+        $filters = ["escape" => 24];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 18
+        $context["classes"] = [0 => "list-group", 1 => ((        // line 20
+($context["compact"] ?? null)) ? ("compact") : (""))];
+        // line 23
+        if (($context["content"] ?? null)) {
+            // line 24
+            echo "  <dl";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method")), "html", null, true);
+            echo ">
+    ";
+            // line 25
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["content"] ?? null));
+            foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                // line 26
+                echo "      <dt class=\"list-group__link\">";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "link", [])), "html", null, true);
+                echo "</dt>
+      ";
+                // line 27
+                if ($this->getAttribute($context["item"], "description", [])) {
+                    // line 28
+                    echo "        <dd class=\"list-group__description\">";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "description", [])), "html", null, true);
+                    echo "</dd>
+      ";
+                }
+                // line 30
+                echo "    ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 31
+            echo "  </dl>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/admin/admin-block-content.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  88 => 31,  82 => 30,  76 => 28,  74 => 27,  69 => 26,  65 => 25,  60 => 24,  58 => 23,  56 => 20,  55 => 18,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/admin/admin-block-content.html.twig", "/var/www/html/eatnshare/core/themes/stable/templates/admin/admin-block-content.html.twig");
+    }
+}
